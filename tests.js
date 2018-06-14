c6e9727a26165f699aa62a4313fef753b8db9fc6
Alexander Pope <alex@pope-industries.com>
2018-06-14 08:54:37 +0200

    replace var with const/let

--- tests.js
+++ tests.js
@@ -1,8 +1,9 @@
 "use strict";
-var ST = require("./stream-template");
-var expect = require("expect.js");
-var concat = require("concat-stream");
-var stream = require("readable-stream");
+
+const ST = require("./stream-template");
+const expect = require("expect.js");
+const concat = require("concat-stream");
+const stream = require("readable-stream");
 
 function makeStream() {
   return new stream.Readable({
@@ -12,7 +13,7 @@ function makeStream() {
 
 describe("stream-template", function() {
   it("should produce a stream as output", function(done) {
-    var out = ST`Hello world`;
+    const out = ST`Hello world`;
     out.pipe(
       concat(function(output) {
         expect(Buffer.isBuffer(output)).equal(true);
@@ -23,9 +24,9 @@ describe("stream-template", function() {
   });
 
   it("should interpolate normal strings ", function(done) {
-    var name = "tom";
-    var test = "string";
-    var out = ST`Hello ${name}, welcome to the ${test} test`;
+    const name = "tom";
+    const test = "string";
+    const out = ST`Hello ${name}, welcome to the ${test} test`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal(
@@ -37,9 +38,9 @@ describe("stream-template", function() {
   });
 
   it("should interpolate buffers ", function(done) {
-    var name = new Buffer("tom", "utf8");
-    var test = new Buffer("buffer", "utf8");
-    var out = ST`Hello ${name}, welcome to the ${test} test`;
+    const name = new Buffer("tom", "utf8");
+    const test = new Buffer("buffer", "utf8");
+    const out = ST`Hello ${name}, welcome to the ${test} test`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal(
@@ -51,9 +52,9 @@ describe("stream-template", function() {
   });
 
   it("should interpolate streams", function(done) {
-    var stream1 = makeStream();
-    var stream2 = makeStream();
-    var out = ST`First ${stream1} then ${stream2}!`;
+    const stream1 = makeStream();
+    const stream2 = makeStream();
+    const out = ST`First ${stream1} then ${stream2}!`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal(
@@ -72,9 +73,9 @@ describe("stream-template", function() {
   });
 
   it("should wait for all data from a stream ", function(done) {
-    var stream1 = makeStream();
+    const stream1 = makeStream();
     stream1.push("a", "utf8");
-    var out = ST`${stream1}`;
+    const out = ST`${stream1}`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal("abcd");
@@ -93,8 +94,8 @@ describe("stream-template", function() {
   });
 
   it("should intepolate promises returning strings", function(done) {
-    var promise = Promise.resolve("hello");
-    var out = ST`${promise} world`;
+    const promise = Promise.resolve("hello");
+    const out = ST`${promise} world`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal("hello world");
@@ -104,9 +105,9 @@ describe("stream-template", function() {
   });
 
   it("should interpolate promises returning streams", function(done) {
-    var stream1 = makeStream();
-    var promise = Promise.resolve(stream1);
-    var out = ST`${promise} world`;
+    const stream1 = makeStream();
+    const promise = Promise.resolve(stream1);
+    const out = ST`${promise} world`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal("hello world");
@@ -118,20 +119,20 @@ describe("stream-template", function() {
   });
 
   it("should interpolate arrays", function(done) {
-    var stream1 = makeStream();
-    var stream2 = makeStream();
+    const stream1 = makeStream();
+    const stream2 = makeStream();
     stream1.push("there ");
     stream2.push("world");
     stream1.push(null);
     stream2.push(null);
-    var array = [
+    const array = [
       "hello ",
       Promise.resolve(stream1),
       stream2,
       [", how's ", "it ", "going"]
     ];
 
-    var out = ST`Well ${array}?`;
+    const out = ST`Well ${array}?`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal(
@@ -143,8 +144,8 @@ describe("stream-template", function() {
   });
 
   it("should concatenate multiple strings together and emit as a single chunk", function(done) {
-    var promise = Promise.resolve("d");
-    var out = ST`a ${"b"} c ${promise} e ${["f", " g"]}`;
+    const promise = Promise.resolve("d");
+    const out = ST`a ${"b"} c ${promise} e ${["f", " g"]}`;
     let chunks = [];
     out.on("data", chunk => chunks.push(chunk.toString()));
     out.on("end", () => {
@@ -154,9 +155,9 @@ describe("stream-template", function() {
   });
 
   it("should allow different encodings to be specified", function(done) {
-    var name = new Buffer("tom", "utf16le");
-    var test = "encoding";
-    var out = ST.encoding(
+    const name = new Buffer("tom", "utf16le");
+    const test = "encoding";
+    const out = ST.encoding(
       "utf16le"
     )`Hello ${name}, welcome to the ${test} test`;
     out.pipe(
@@ -170,8 +171,8 @@ describe("stream-template", function() {
   });
 
   it("should forward stream errors", function(done) {
-    var s = makeStream();
-    var out = ST`${s}`;
+    const s = makeStream();
+    const out = ST`${s}`;
 
     out.on("error", function(err) {
       expect(err.message).equal("destroyed");
@@ -182,8 +183,8 @@ describe("stream-template", function() {
   });
 
   it("should destroy source streams on error", function(done) {
-    var destroyed = false;
-    var s = stream.Readable({
+    let destroyed = false;
+    const s = stream.Readable({
       read: function() {},
       destroy: function() {
         destroyed = true;
@@ -191,7 +192,7 @@ describe("stream-template", function() {
       }
     });
 
-    var out = ST`${s}`;
+    const out = ST`${s}`;
 
     s.on("close", function() {
       expect(destroyed).equal(true);
@@ -202,8 +203,8 @@ describe("stream-template", function() {
   });
 
   it("should destroy source streams on Promise error", function(done) {
-    var promise = Promise.reject(Error("destroyed"));
-    var out = ST`${promise} world`;
+    const promise = Promise.reject(Error("destroyed"));
+    const out = ST`${promise} world`;
 
     out.on("error", function(err) {
       expect(err.message).equal("destroyed");
@@ -212,9 +213,9 @@ describe("stream-template", function() {
   });
 
   it("should handle undefined interpolations", function(done) {
-    var name = "tom";
-    var test = undefined;
-    var out = ST`Hello ${name}, welcome to the ${test} test`;
+    const name = "tom";
+    const test = undefined;
+    const out = ST`Hello ${name}, welcome to the ${test} test`;
     out.pipe(
       concat(function(output) {
         expect(output.toString()).equal("Hello tom, welcome to the  test");
